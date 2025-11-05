62e7305029d5826d34e38ad45ff898c1f482b309
Fefemyster <kenennlara@gmail.com>
2025-11-05 12:25:26 -0600

    Creando arreglo Productos

--- Inventario/inventario-typescript/src/pruebas.ts
+++ Inventario/inventario-typescript/src/pruebas.ts
@@ -10,3 +10,35 @@ import {
     agregarStock,
     totalizarPorCategoria
 } from './inventario.ts'
+
+const arregloProductos: Producto [] =[
+    { id: 1,
+    nombre: 'Laptop',
+    precio: 2000 ,
+    categoria: 'Electrónica',
+    stock: 20,
+    proveedor: 'Dell'},
+    
+    { id: 2,
+    nombre: 'Telefono',
+    precio: 2000 ,
+    categoria: 'Electrónica',
+    stock: 10,
+    proveedor: 'Samsung'},
+
+    { id: 3,
+    nombre: 'Camisa Polo',
+    precio: 2000 ,
+    categoria: 'Textil',
+    stock: 5,
+    proveedor: 'Pacer'},
+
+    { id: 4,
+    nombre: 'Pollo Entero',
+    precio: 2000 ,
+    categoria: 'Alimentos',
+    stock: 40,
+    proveedor: 'Pollo Rey'},
+]   
+
+console.log(arregloProductos);
