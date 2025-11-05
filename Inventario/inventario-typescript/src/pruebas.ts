e68e98fef675239902203f81deb76cb116d95672
Fefemyster <kenennlara@gmail.com>
2025-11-05 10:23:39 -0600

    Importando funcions y estructuras

--- Inventario/inventario-typescript/src/pruebas.ts
+++ Inventario/inventario-typescript/src/pruebas.ts
@@ -1,3 +1,12 @@
 console.log('Hello World');
 
 
+import {
+    type Producto,
+    type tipoCategoria,
+    type estadoInventario,
+    type reporteProducto,
+    clasificarInventario,
+    agregarStock,
+    totalizarPorCategoria
+} from './inventario.ts'
